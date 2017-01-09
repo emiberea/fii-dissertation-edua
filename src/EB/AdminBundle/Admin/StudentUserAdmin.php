d38b3691e9c319de0a092ebd5ca3260b7a0278da
Emi Berea <emi.berea@gmail.com>
2017-01-09 12:19:52 +0200

    Add admin area CRUDS for all 3 user entities.

---
+++ src/EB/AdminBundle/Admin/StudentUserAdmin.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace EB\AdminBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class StudentUserAdmin extends AbstractAdmin
+{
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('firstName', 'text')
+            ->add('lastName', 'text')
+            ->add('fatherInitial')
+            ->add('pin')
+            ->add('city')
+            ->add('address')
+            ->add('highSchool')
+            ->add('specialization')
+            ->add('admissionExamGrade')
+            ->add('baccalaureateAverageGrade')
+            ->add('baccalaureateMaximumGrade')
+        ;
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('firstName')
+            ->add('lastName')
+            ->add('fatherInitial')
+            ->add('pin')
+            ->add('city')
+            ->add('address')
+            ->add('highSchool')
+            ->add('specialization')
+            ->add('admissionExamGrade')
+            ->add('baccalaureateAverageGrade')
+            ->add('baccalaureateMaximumGrade')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('firstName')
+            ->addIdentifier('lastName')
+            ->add('fatherInitial')
+            ->add('pin')
+            ->add('city')
+            ->add('address')
+            ->add('highSchool')
+            ->add('specialization')
+            ->add('admissionExamGrade')
+            ->add('baccalaureateAverageGrade')
+            ->add('baccalaureateMaximumGrade')
+        ;
+    }
+}
