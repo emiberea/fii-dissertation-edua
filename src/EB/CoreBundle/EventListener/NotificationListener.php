640a664a536d812568f24392a44126f1de836a60
Emi Berea <emi.berea@gmail.com>
2017-01-19 14:13:27 +0200

    Updates on NotificationListener, added basic implementation of methods, tests needed.

--- src/EB/CoreBundle/EventListener/NotificationListener.php
+++ src/EB/CoreBundle/EventListener/NotificationListener.php
@@ -3,9 +3,13 @@
 namespace EB\CoreBundle\EventListener;
 
 use Doctrine\ORM\EntityManager;
+use EB\CoreBundle\Entity\Admission;
+use EB\CoreBundle\Entity\Notification;
 use EB\CoreBundle\Event\NotificationEvent;
 use EB\CoreBundle\Event\NotificationEvents;
 use EB\CoreBundle\Service\MailerService;
+use EB\UserBundle\Entity\SchoolStaffUser;
+use EB\UserBundle\Entity\StudentUser;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Routing\Router;
 
@@ -21,7 +25,6 @@ class NotificationListener implements EventSubscriberInterface
     protected $mailer;
 
     /**
-     * NotificationListener constructor.
      * @param EntityManager $em
      * @param Router $router
      * @param MailerService $mailer
@@ -42,16 +45,99 @@ class NotificationListener implements EventSubscriberInterface
         ];
     }
 
+    /**
+     * @param NotificationEvent $event
+     * @todo test this method to be sure it works as intended
+     */
     public function onStudentRegistered(NotificationEvent $event)
     {
-        // TODO: implement stuff
+        // send notification and email to all admin users
+        /** @var StudentUser $student */
+        $student = $event->get('student');
+
+        $admins = $this->em->getRepository('EBUserBundle:AdminUser')->findAll();
+        foreach ($admins as $admin) {
+            $this->mailer->sendEmail($admin->getEmail(), 'EBCoreBundle:Email:studentRegistered.html.twig' , [
+                'student' => $student,
+                'admin' => $admin,
+            ]);
+
+            $notification = new Notification();
+            $notification
+                ->setType(Notification::TYPE_STUDENT_REGISTERED)
+                ->setSenderUser($student)
+                ->setReceiverUser($admin)
+            ;
+
+            $this->em->persist($notification);
+        }
+
+        $this->em->flush();
     }
 
+    /**
+     * @param NotificationEvent $event
+     * @todo test this method to be sure it works as intended
+     */
     public function onStudentAttendAdmission(NotificationEvent $event)
     {
+        // send notification and email to all Ssu (school staff users) that are assigned to that school,
+        /** @var StudentUser $student */
+        $student = $event->get('student');
+        /** @var Admission $admission */
+        $admission = $event->get('admission');
+
+        $schoolStaffUsers = $this->em->getRepository('EBUserBundle:SchoolStaffUser')->findBy([
+            'school' => $admission->getSchool(),
+        ]);
+        foreach ($schoolStaffUsers as $schoolStaffUser) {
+            $this->mailer->sendEmail($schoolStaffUser->getEmail(), 'EBCoreBundle:Email:studentAttendAdmission.html.twig' , [
+                'student' => $student,
+                'schoolStaffUser' => $schoolStaffUser,
+                'admission' => $admission,
+            ]);
+
+            $notification = new Notification();
+            $notification
+                ->setType(Notification::TYPE_STUDENT_ATTEND_ADMISSION)
+                ->setSenderUser($student)
+                ->setReceiverUser($schoolStaffUser)
+            ;
+
+            $this->em->persist($notification);
+        }
+
+        $this->em->flush();
     }
 
+    /**
+     * @param NotificationEvent $event
+     * @todo test this method to be sure it works as intended
+     */
     public function onSsuConfirmStudent(NotificationEvent $event)
     {
+        // send notification and email to the student, that he was accepted by a Ssu
+        /** @var StudentUser $student */
+        $student = $event->get('student');
+        /** @var SchoolStaffUser $schoolStaffUser */
+        $schoolStaffUser = $event->get('schoolStaffUser');
+        /** @var Admission $admission */
+        $admission = $event->get('admission');
+
+        $this->mailer->sendEmail($schoolStaffUser->getEmail(), 'EBCoreBundle:Email:ssuConfirmStudent.html.twig' , [
+            'student' => $student,
+            'schoolStaffUser' => $schoolStaffUser,
+            'admission' => $admission,
+        ]);
+
+        $notification = new Notification();
+        $notification
+            ->setType(Notification::TYPE_SSU_CONFIRM_STUDENT)
+            ->setSenderUser($schoolStaffUser)
+            ->setReceiverUser($student)
+        ;
+
+        $this->em->persist($notification);
+        $this->em->flush();
     }
 }
